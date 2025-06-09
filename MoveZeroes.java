865fb78fee4162b4d2f556049e93db03a3a43cd2
m1xl30 <makspavlov.by@gmail.com>
2025-06-09 20:25:15 +0200

    move zeroes with a 1ms complexity

---
+++ MoveZeroes.java
@@ -0,0 +1,11 @@
+public class MoveZeroes {
+    //Task:
+    //https://leetcode.com/problems/move-zeroes/?envType=study-plan-v2&envId=leetcode-75
+
+
+
+    
+    public static void main(String[] args) {
+
+    }
+}
